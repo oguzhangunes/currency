b271010e4b1c3c93edbaf23f669d7c17132bbc45
Oguzhan Günes <49681375+oguzhangunes@users.noreply.github.com>
2019-07-04 17:14:12 +0200

    Update currency.py

--- currency.py
+++ currency.py
@@ -102,7 +102,7 @@ class currency():
         print (day_count)
         if day_count > 0:
             for i in range(0, day_count):
-
+                print(i)
                 v_date = date.today() - timedelta(days=i)
                 v_date = v_date.strftime('%Y-%m-%d')
                 print (v_date)
